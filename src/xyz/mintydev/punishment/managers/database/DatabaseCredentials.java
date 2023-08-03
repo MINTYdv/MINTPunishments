f6846df43c9442a1e4a39e4174c734e711c80292
MINTY <lmurte@hotmail.com>
2023-08-03 22:55:04 +0200

    Database work

--- src/xyz/mintydev/punishment/managers/database/DatabaseCredentials.java
+++ src/xyz/mintydev/punishment/managers/database/DatabaseCredentials.java
@@ -6,21 +6,44 @@ package xyz.mintydev.punishment.managers.database;
 public class DatabaseCredentials {
 
 	private final String ip,database,user,password;
+	private final int port;
 
-	public DatabaseCredentials(String ip, String database, String user, String password) {
+	public DatabaseCredentials(String ip, String database, String user, String password, int port) {
 		this.ip = ip;
 		this.database = database;
 		this.user = user;
 		this.password = password;
+		this.port = port;
 	}
 
 	public boolean isFilled() {
-		return ip != null && ip.length() > 5 && database != null && database.length() > 0 && user != null && user.length() > 0 && password != null && password.length() > 0;
+		return port != 0 && ip != null && ip.length() > 5 && database != null && database.length() > 0 && user != null && user.length() > 0 && password != null && password.length() > 0;
+	}
+	
+	/** 
+	 * Function called to transform the database credentials
+	 * to an URI.
+	 * */
+	public String toURI() {
+		final StringBuilder sb = new StringBuilder();
+		
+		sb.append("jdbc:mysql://")
+			.append(ip)
+			.append(":")
+			.append(port)
+			.append("/")
+			.append(database);
+	
+		return sb.toString();
 	}
 	
 	/* 
 	 * Getters & Setters
 	 * */
+	
+	public int getPort() {
+		return port;
+	}
 	
 	public String getIp() {
 		return ip;
--- src/xyz/mintydev/punishment/managers/database/DatabaseManager.java
+++ src/xyz/mintydev/punishment/managers/database/DatabaseManager.java
@@ -1,5 +1,13 @@
 package xyz.mintydev.punishment.managers.database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.bukkit.configuration.ConfigurationSection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import xyz.mintydev.punishment.MINTPunishment;
 
 public class DatabaseManager {
@@ -7,14 +15,84 @@ public class DatabaseManager {
 	private final MINTPunishment main;
 	private static DatabaseManager instance;
 	
+	private DatabaseCredentials credentials;
+	private HikariDataSource hikariDataSource;
+	private HikariConfig hikariConfig;
+	
 	public DatabaseManager(MINTPunishment main) {
 		this.main = main;
 		instance = this;
+		
+		credentials = loadCredentials();
+		setup();
+	}
+	
+	/** 
+	 * Function called to setup the database/hikari database connection
+	 * */
+	private void setup() {
+		if(hikariConfig != null) return;
+		
+		hikariConfig = new HikariConfig();
+		
+		hikariConfig.setMaximumPoolSize(10);
+		hikariConfig.setJdbcUrl(credentials.toURI());
+		
+		hikariConfig.setPassword(credentials.getPassword());
+		hikariConfig.setUsername(credentials.getUser());
+		
+		hikariConfig.setMaxLifetime(10*60*1000);
+		hikariConfig.setIdleTimeout(5*60*1000);
+		hikariConfig.setLeakDetectionThreshold(5*60*1000);
+		hikariConfig.setConnectionTimeout(60*1000);
+		
+		this.hikariDataSource = new HikariDataSource(hikariConfig);
+	}
+	
+	public void initPool() {
+		setup();
+	}
+	
+	public void closePool() {
+		this.hikariDataSource.close();
+	}
+	
+	public Connection getConnection() {
+		if(this.hikariDataSource == null) {
+			setup();
+		}
+		
+		try {
+			return this.hikariDataSource.getConnection();
+		} catch (SQLException e) {
+			main.getLogger().warning("ERROR : Could not connect to database.");
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/** 
+	 * Function called to load database credentials from the config.yml file
+	 * */
+	private DatabaseCredentials loadCredentials() {
+		final ConfigurationSection sec = main.getConfig().getConfigurationSection("mysql");
+		final String ip = sec.getString("ip"); 
+		final String database = sec.getString("database"); 
+		final String user = sec.getString("user"); 
+		final String password = sec.getString("password"); 
+		
+		final int port = sec.getInt("port");
+		
+		return new DatabaseCredentials(ip, database, user, password, port);
 	}
 
 	/* 
 	 * Getters & Setters
 	 * */
+	
+	public DatabaseCredentials getCredentials() {
+		return credentials;
+	}
 	
 	public static DatabaseManager get() {
 		return instance;
