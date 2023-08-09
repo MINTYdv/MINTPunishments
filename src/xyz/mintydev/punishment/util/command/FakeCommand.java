b45e3a2631f6dec8caac98e30b09248dd4d46fdd
MINTY <lmurte@hotmail.com>
2023-08-09 19:16:15 +0200

    🧹 + Added JavaDoc

--- src/xyz/mintydev/punishment/util/command/FakeCommand.java
+++ src/xyz/mintydev/punishment/util/command/FakeCommand.java
@@ -3,6 +3,7 @@ package xyz.mintydev.punishment.util.command;
 import org.bukkit.command.CommandSender;
 
 import xyz.mintydev.punishment.MINTPunishment;
+import xyz.mintydev.punishment.managers.CommandManager;
 
 public class FakeCommand extends org.bukkit.command.Command {
 
@@ -10,6 +11,9 @@ public class FakeCommand extends org.bukkit.command.Command {
 		super(name);
 	}
 
+	/** 
+	 * Returns the code to the {@link CommandManager}
+	 * */
 	@Override
 	public boolean execute(CommandSender sender, String commandLabel, String[] args) {
 		return MINTPunishment.get().getCommandManager().onCommand(sender, this, commandLabel, args);
